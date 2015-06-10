2c6dc83b117b022d18aab17f83677b91496b0cf8
connyliu <appcan@AppCandeMac-mini.local>
2015-06-10 10:00:37 +0800

    修复了选择器会被错误关闭的bug

--- EUExAreaPickerView/EUExAreaPickerView/EUExAreaPickerView.m
+++ EUExAreaPickerView/EUExAreaPickerView/EUExAreaPickerView.m
@@ -12,10 +12,23 @@
 #import "EUtility.h"
 #import "JSON.h"
 @interface EUExAreaPickerView() <LDAreaPickerDelegate>
-@property(nonatomic,strong)UITextField *pseudoTextField;
+//@property(nonatomic,strong)UITextField *pseudoTextField;
 @property(nonatomic,strong)LDAreaPickerView *areaPicker;
+@property(nonatomic,strong)UIButton *clearField;
+@property(nonatomic,strong)UIButton *confirmButton;
+@property(nonatomic,strong)UIImageView *confirmField;
+
 @end
 @implementation EUExAreaPickerView
+
+const CGFloat kPickerHeight = 216.f;
+const CGFloat kComfirmButtonHeight = 40.f;
+const CGFloat kComfirmButtonWidth=60.f;
+
+
+
+
+
 
 -(id)initWithBrwView:(EBrowserView *)eInBrwView{
     self = [super initWithBrwView:eInBrwView];
@@ -24,14 +37,41 @@
     return self;
 }
 
+-(void)clean{
+    if(self.areaPicker){
+        [self.areaPicker removeFromSuperview];
+        [self.areaPicker release];
+        self.areaPicker=nil;
+    }
+    if(self.clearField){
+        [self.clearField removeFromSuperview];
+        [self.clearField release];
+        self.clearField=nil;
+    }
+    if(self.confirmField){
+        [self.confirmField removeFromSuperview];
+        [self.confirmField release];
+        self.confirmField=nil;
+    }
+    if(self.confirmButton){
+        [self.confirmButton removeFromSuperview];
+        [self.confirmButton release];
+        self.confirmButton=nil;
+    }
+}
+
+-(void)dealloc{
+    [self clean];
+    [super dealloc];
+}
 -(void)open:(NSMutableArray *)array{
   //  if ([array isKindOfClass:[NSMutableArray class]] && [array count]>0) {
      //   CGFloat x = [[array objectAtIndex:0] floatValue];
        // CGFloat y = [[array objectAtIndex:1] floatValue];
       //  CGFloat w = [[array objectAtIndex:2] floatValue];
       //  CGFloat h = [[array objectAtIndex:3] floatValue];
-    CGFloat h=0;
- 
+    //CGFloat h=0;
+ /*
         if (!self.pseudoTextField) {
             self.pseudoTextField = [[UITextField alloc] initWithFrame:CGRectZero];
             [EUtility brwView:meBrwView addSubview:self.pseudoTextField];
@@ -45,6 +85,7 @@
             [keyboardDoneButtonView setItems:[NSArray arrayWithObjects:spaceBtn,doneButton, nil]];
             self.pseudoTextField.inputAccessoryView = keyboardDoneButtonView;
         }
+    
         if (!self.areaPicker) {
             self.areaPicker = [[LDAreaPickerView alloc] initWithFrame:CGRectMake(0, [EUtility screenHeight] - h, [EUtility screenWidth], h) Style:LDAreaPickerWithStateAndCityAndDistrict delegate:self];
             
@@ -54,6 +95,53 @@
         [self.pseudoTextField becomeFirstResponder];
         
    // }
+  */
+    [self setup];
+    [self showAll];
+    
+}
+
+-(void)setup{
+    CGFloat width=[EUtility screenWidth];
+    CGFloat clearHeight=[EUtility screenHeight]-kComfirmButtonHeight-kPickerHeight;
+    
+    if(!self.clearField){
+        self.clearField=[UIButton buttonWithType:UIButtonTypeCustom];
+        [self.clearField setFrame:CGRectMake(0, 0, width, clearHeight)];
+        self.clearField.opaque=NO;
+        //self.clearField.alpha=0;
+        self.clearField.backgroundColor=[UIColor clearColor];
+        self.clearField.showsTouchWhenHighlighted=NO;
+        [self.clearField addTarget:self action:@selector(closeAll) forControlEvents:UIControlEventTouchUpInside];
+        //self.clearField.
+    }
+    if(!self.confirmField){
+        self.confirmField=[[UIImageView alloc]initWithFrame:CGRectMake(0, clearHeight,width, kComfirmButtonHeight)];
+        self.confirmField.backgroundColor=[UIColor whiteColor];
+    }
+    if(!self.confirmButton){
+        self.confirmButton=[UIButton buttonWithType:UIButtonTypeCustom];
+        [self.confirmButton setFrame:CGRectMake(width-kComfirmButtonWidth,clearHeight,kComfirmButtonWidth, kComfirmButtonHeight)];
+
+        self.confirmButton.backgroundColor=[UIColor clearColor];
+        self.confirmButton.showsTouchWhenHighlighted=NO;
+        [self.confirmButton setTitle:@"确定" forState:UIControlStateNormal];
+        [self.confirmButton setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+        [self.confirmButton setTitle:@"确定" forState:UIControlStateSelected];
+        [self.confirmButton setTitleColor:[UIColor blackColor] forState:UIControlStateSelected];
+        [self.confirmButton addTarget:self action:@selector(clickBtnDone) forControlEvents:UIControlEventTouchUpInside];
+    }
+    if (!self.areaPicker) {
+        self.areaPicker = [[LDAreaPickerView alloc] initWithFrame:CGRectMake(0,clearHeight+kComfirmButtonHeight, width,kPickerHeight) Style:LDAreaPickerWithStateAndCityAndDistrict delegate:self];
+        
+        
+    }
+}
+-(void)showAll{
+    [EUtility brwView:meBrwView addSubview:self.clearField];
+    [EUtility brwView:meBrwView addSubview:self.confirmField];
+    [EUtility brwView:meBrwView addSubview:self.confirmButton];
+    [EUtility brwView:meBrwView addSubview:self.areaPicker];
 }
 - (void)pickerDidChaneStatus:(LDAreaPickerView *)picker{
     NSString *resultStr = nil;
@@ -63,12 +151,33 @@
         resultStr = [NSString stringWithFormat:@"%@ %@", picker.locate.state, picker.locate.city];
     }
 }
+-(void)closeAll{
+    if(self.areaPicker){
+        [self.areaPicker removeFromSuperview];
+        
+    }
+    if(self.clearField){
+        [self.clearField removeFromSuperview];
+        
+    }
+    if(self.confirmField){
+        [self.confirmField removeFromSuperview];
+        
+    }
+    if(self.confirmButton){
+        [self.confirmButton removeFromSuperview];
+    }
+
+}
+
 -(void)close:(NSMutableArray *)array{
-    if (self.pseudoTextField) {
+  /*  if (self.pseudoTextField) {
         [self.pseudoTextField resignFirstResponder];
         [self.pseudoTextField removeFromSuperview];
         self.pseudoTextField = nil;
     }
+   */
+    [self closeAll];
 }
 -(void)clickBtnDone{
     NSString *resultStr = nil;
@@ -80,6 +189,7 @@
     NSString *str =[NSString stringWithFormat:@"uexAreaPickerView.onConfirmClick('%@');",[@{@"city":resultStr} JSONRepresentation]];
     
     [self.meBrwView stringByEvaluatingJavaScriptFromString:str];
+    [self closeAll];
 
 }
 @end
